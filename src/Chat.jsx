7fd3f988f8d1785daba58f266e653e822117de6a
Руслан Королев <techfusionwork@gmail.com>
2019-04-13 22:18:58 +0300

    add some logic to reducer

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,29 +1,44 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { MainContext } from './Context';
 import s from './Chat.module.scss';
 import cx from 'classnames';
 
 const Telegram = () => {
-    const {state, dispatch} = useContext(MainContext);
+    const { state, dispatch } = useContext(MainContext);
+
+    const addAnswers = (e) => {
+        console.log('value:', e.target.innerText);
+        dispatch({ type: 'ADD_ANSWER', payload: { text: e.target.innerText, index: 0 } });
+        dispatch({ type: 'GET_NEXT_STAGE' });
+    };
+
     return (
         <div className={s.chat}>
-            {console.log('context:', state, dispatch)}
+            {console.log('context:', state)}
             <div className={s.messages}>
-                {state.messages.map(message =>
-                    <div className={s.message}>
-                        <p className={s.text}>
-                            {message.text}
-                        </p>
-                    </div>
-                )}
-                <div className={cx(s.message, s.answerMessage)}>
-                    <p className={s.text}>
-                        Я не хочу у вас работать!
-                    </p>
-                </div>
+                {state.chat.map(item => {
+                    {
+                        console.log(':', item)
+                    }
+                    return (
+                        <Fragment>
+                            {item.message && item.message.text && <div className={s.message}>
+                                <p className={s.text}>
+                                    {item.message && item.message.text}
+                                </p>
+                            </div>}
+                            {item.answer && item.answer.text &&
+                            <div className={cx(s.message, s.answerMessage)}>
+                                <p className={s.text}>
+                                    {item.answer && item.answer.text}
+                                </p>
+                            </div>}
+                        </Fragment>)
+                })}
             </div>
             <div className={s.answerSection}>
-                <div className={s.possibleAnswer}><p className={s.text} onClick={() => dispatch({ type: 'ADD_MESSAGE'})}>Я не хочу у вас работать!</p></div>
+                <div className={s.possibleAnswer}><p className={s.text} onClick={(e) => addAnswers(e)}>Я не хочу у вас
+                    работать!</p></div>
                 <div className={s.possibleAnswer}><p className={s.text}>Сколько ЗП дадите?</p></div>
                 <div className={s.possibleAnswer}><p className={s.text}>Дашь мне свой номер?</p></div>
             </div>
--- src/Context.js
+++ src/Context.js
@@ -1,14 +1,49 @@
 import React, { useReducer } from 'react'
 
-const initialState = { messages: [{text: 'Добрый день. Начнем нашу игру?'}] };
+const initialState = { stage: 0, chat: [{ message: {text: 'Добрый день. Начнем нашу игру?'}}, { answer: {}}] };
 const MainContext = React.createContext(initialState);
 
+const stages = {
+    0: {
+        answer: {
+            0: {
+                message: { text: 'Ну и пошел ты на хер!' }
+            }
+        }
+    }
+};
+
 const reducer = (state, action) => {
+    console.log('ACTION:', action.type, state, action);
     switch (action.type) {
         case 'ADD_MESSAGE':
             return {
                 ...state,
-                messages: [{text: 'Я вам не писала потому что думала что вы крутой разработчик'}]
+                chat: [
+                    { message: [{text: 'Я вам не писала потому что думала что вы крутой разработчик'}] }
+                ]
+            };
+
+        case 'ADD_ANSWER':
+            return {
+                ...state,
+                answer: { text: action.payload.text, index: 0 }
+            };
+
+        case 'GET_NEXT_STAGE':
+            console.log('stateanswer:', state.answer);
+            return {
+                ...state,
+                chat: [
+                    ...state.chat,
+                    {
+                        answer: state.answer
+                    },
+                    {
+                        message: stages[0].answer[0].message
+                    }
+                ],
+                stage: state.stage + 1
             };
         default:
             return state;
